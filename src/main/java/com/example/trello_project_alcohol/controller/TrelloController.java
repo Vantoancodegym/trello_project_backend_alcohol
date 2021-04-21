b69755164997b89bb25d9c9bb120e1a08f11d864
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-21 15:43:27 +0700

    create API create new list in board

--- src/main/java/com/example/trello_project_alcohol/controller/TrelloController.java
+++ src/main/java/com/example/trello_project_alcohol/controller/TrelloController.java
@@ -1,9 +1,23 @@
 package com.example.trello_project_alcohol.controller;
 
+import com.example.trello_project_alcohol.DTO.ResultResponse;
+import com.example.trello_project_alcohol.model.List;
+import com.example.trello_project_alcohol.service.listService.IListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
 public class TrelloController {
+    @Autowired
+    private IListService listService;
+    @PostMapping("createList")
+    public ResponseEntity<?> createList(@RequestBody List list){
+        return new  ResponseEntity<>(new ResultResponse("Create obj List success"), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/trello_project_alcohol/repo/TagUser_Board_Repo.java
+++ src/main/java/com/example/trello_project_alcohol/repo/TagUser_Board_Repo.java
@@ -1,8 +1,9 @@
 package com.example.trello_project_alcohol.repo;
 
+import com.example.trello_project_alcohol.model.TagUser_Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TagUser_Board_Repo extends JpaRepository<TagUser_Board_Repo,Long> {
+public interface TagUser_Board_Repo extends JpaRepository<TagUser_Board,Long> {
 }
