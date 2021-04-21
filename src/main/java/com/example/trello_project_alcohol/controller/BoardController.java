b418cc246ecc17afd9d9f1076e8a2856e6ef3c8e
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2021-04-21 21:19:17 +0700

    edit controller

--- src/main/java/com/example/trello_project_alcohol/controller/BoardController.java
+++ src/main/java/com/example/trello_project_alcohol/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.example.trello_project_alcohol.service.broad.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +16,11 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @GetMapping("")
     public ResponseEntity <List<Board>> showAll(){
         return new ResponseEntity<>(boardService.findAll(), HttpStatus.OK);
     }
+    @PostMapping("create")
     public ResponseEntity<Board> create(@RequestBody Board board){
         return new ResponseEntity<>(boardService.save(board), HttpStatus.OK);
     }
