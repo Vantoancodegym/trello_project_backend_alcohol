1d8731ba4f378a1caae38190f18995f0ffd7b6f7
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-21 11:36:28 +0700

    create appRole model

--- src/main/java/com/example/trello_project_alcohol/model/AppUser.java
+++ src/main/java/com/example/trello_project_alcohol/model/AppUser.java
@@ -6,70 +6,81 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
 @Entity
 public class AppUser {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        @Size(min = 3, max = 30)
-        private String userName;
-        private String passWord;
-        private String avatar;
-        @Transient
-        private MultipartFile avatarMul;
-        @Email
-        @NotBlank
-        private String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min = 3, max = 30)
+    private String userName;
+    private String passWord;
+    private String avatar;
+    @Transient
+    private MultipartFile avatarMul;
+    @Email
+    @NotBlank
+    private String email;
+    @OneToOne
+    private AppRole appRole;
 
-        public AppUser() {
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public String getPassWord() {
-            return passWord;
-        }
-
-        public void setPassWord(String passWord) {
-            this.passWord = passWord;
-        }
-
-        public String getAvatar() {
-            return avatar;
-        }
-
-        public void setAvatar(String avatar) {
-            this.avatar = avatar;
-        }
-
-        public MultipartFile getAvatarMul() {
-            return avatarMul;
-        }
-
-        public void setAvatarMul(MultipartFile avatarMul) {
-            this.avatarMul = avatarMul;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public AppUser() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public MultipartFile getAvatarMul() {
+        return avatarMul;
+    }
+
+    public void setAvatarMul(MultipartFile avatarMul) {
+        this.avatarMul = avatarMul;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AppRole getAppRole() {
+        return appRole;
+    }
+
+    public void setAppRole(AppRole appRole) {
+        this.appRole = appRole;
+    }
+}
 
