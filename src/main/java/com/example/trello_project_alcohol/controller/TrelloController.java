3c04170d1207a5509655e6ba5cb565c61625bd5d
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-21 15:20:07 +0700

    create trello controller

---
+++ src/main/java/com/example/trello_project_alcohol/controller/TrelloController.java
@@ -0,0 +1,9 @@
+package com.example.trello_project_alcohol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class TrelloController {
+}
