c6153b82b97405ef49c2ee5c590e54705090ed85
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-21 11:47:54 +0700

    add model TagUser_Board

---
+++ src/main/java/com/example/trello_project_alcohol/model/TagUser_Board.java
@@ -0,0 +1,45 @@
+package com.example.trello_project_alcohol.model;
+
+import org.springframework.security.core.userdetails.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class TagUser_Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Board board;
+    @OneToMany
+    private List<AppUser> appUser;
+
+    public TagUser_Board() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public List<AppUser> getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(List<AppUser> appUser) {
+        this.appUser = appUser;
+    }
+}
+
