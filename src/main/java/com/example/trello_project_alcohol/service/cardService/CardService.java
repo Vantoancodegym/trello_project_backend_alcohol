2c40e5f4c515a87c6be97b52cfaf46f3deb71464
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-22 14:00:57 +0700

    create CardService

---
+++ src/main/java/com/example/trello_project_alcohol/service/cardService/CardService.java
@@ -0,0 +1,37 @@
+package com.example.trello_project_alcohol.service.cardService;
+
+import com.example.trello_project_alcohol.model.Card;
+import com.example.trello_project_alcohol.repo.CardRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CardService implements ICardService{
+    @Autowired
+    private CardRepo cardRepo;
+    @Override
+    public List<Card> findAll() {
+        return null;
+    }
+
+    @Override
+    public Card findById(Long id) {
+        return cardRepo.findById(id).get();
+    }
+
+    @Override
+    public Card save(Card card) {
+        return cardRepo.save(card);
+    }
+
+    @Override
+    public void delete(Long id) {
+        cardRepo.deleteById(id);
+
+    }
+
+    @Override
+    public List<Card> findCardsByListId(Long id) {
+        return cardRepo.findCardsByList_IdOrOrderByPosition(id);
+    }
+}
