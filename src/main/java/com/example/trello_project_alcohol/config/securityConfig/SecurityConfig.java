56eef17846b0ac262e4ed41b1e5bb9fc7c847215
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-22 14:25:41 +0700

    edit SecurityConfig allow all

--- src/main/java/com/example/trello_project_alcohol/config/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/trello_project_alcohol/config/securityConfig/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/**");
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
