c84cf3930457c4e799cb4f674e41fdda5446f6e6
Ngô Minh Hiếu <hieunm92@gmail.com>
2021-04-22 15:59:12 +0700

    method createUser

--- src/main/java/com/example/trello_project_alcohol/controller/UserController.java
+++ src/main/java/com/example/trello_project_alcohol/controller/UserController.java
@@ -1,2 +1,26 @@
-package com.example.trello_project_alcohol.controller;public class UserController {
+package com.example.trello_project_alcohol.controller;
+
+import com.example.trello_project_alcohol.model.AppUser;
+import com.example.trello_project_alcohol.service.userService.IAppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class UserController {
+    @Autowired
+    private IAppUserService iAppUserService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<AppUser> createUser(@RequestBody AppUser appUser) {
+        return new ResponseEntity<>(iAppUserService.save(appUser), HttpStatus.OK);
+    }
+
 }
