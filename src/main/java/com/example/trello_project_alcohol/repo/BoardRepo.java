696a8821f9bd4a83870d9f63bfab376f905df998
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2021-04-23 09:45:37 +0700

    add file

--- src/main/java/com/example/trello_project_alcohol/repo/BoardRepo.java
+++ src/main/java/com/example/trello_project_alcohol/repo/BoardRepo.java
@@ -2,8 +2,13 @@ package com.example.trello_project_alcohol.repo;
 
 import com.example.trello_project_alcohol.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepo extends JpaRepository<Board,Long> {
+    @Query(value = "select b.id as id, b.name as name from board as b join app_user au on au.id = b.app_user_id where app_user_id =?", nativeQuery = true)
+    List<Board> findAllByNameByTagAppUserBoard(Long app_user_id);
 }
