79c38a77d403df11f00d7d66c84390167263811d
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-25 21:08:29 +0700

    add MediaFileService

---
+++ src/main/java/com/example/trello_project_alcohol/service/mediaFileService/MediaFileService.java
@@ -0,0 +1,36 @@
+package com.example.trello_project_alcohol.service.mediaFileService;
+
+import com.example.trello_project_alcohol.model.MediaFile;
+import com.example.trello_project_alcohol.repo.MediaFile_repo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class MediaFileService implements IMediaFileService{
+    @Autowired
+    private MediaFile_repo mediaFile_repo;
+    @Override
+    public List<MediaFile> findAll() {
+        return (List<MediaFile>) mediaFile_repo.findAll();
+    }
+
+    @Override
+    public MediaFile findById(Long id) {
+        return mediaFile_repo.findById(id).get();
+    }
+
+    @Override
+    public MediaFile save(MediaFile mediaFile) {
+        return mediaFile_repo.save(mediaFile);
+    }
+
+    @Override
+    public void delete(Long id) {
+        mediaFile_repo.deleteById(id);
+    }
+
+    @Override
+    public List<MediaFile> findMediaFileByCard(Long card_id) {
+        return mediaFile_repo.findMediaFileByCard_Id(card_id);
+    }
+}
