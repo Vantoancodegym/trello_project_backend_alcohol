10741add0cd60dc4c25348bf7036fb8b73c3f5a0
MungC2010G2 <zhaiduong26z@gmail.com>
2021-05-03 21:13:00 +0700

    create method findAll Label

--- src/main/java/com/example/trello_project_alcohol/service/labels/ILabelsService.java
+++ src/main/java/com/example/trello_project_alcohol/service/labels/ILabelsService.java
@@ -10,5 +10,5 @@ public interface ILabelsService extends IService<Labels> {
     List<Labels> findListLabelsByCardId(Long card_id);
     List<Labels> findListSelected(Long card_id);
     void addNewLabelToCard(Card_Labels card_labels);
-
+    List<Labels> showAllLabel();
 }
