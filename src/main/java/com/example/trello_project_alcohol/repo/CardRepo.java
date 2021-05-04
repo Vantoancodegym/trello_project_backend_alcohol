15c816024f1b07326c792362be39dfa89d7d3819
MungC2010G2 <zhaiduong26z@gmail.com>
2021-05-04 19:45:45 +0700

    fix bug query findCardByUserIdAndBoardId

--- src/main/java/com/example/trello_project_alcohol/repo/CardRepo.java
+++ src/main/java/com/example/trello_project_alcohol/repo/CardRepo.java
@@ -10,15 +10,11 @@ import java.util.List;
 @Repository
 public interface CardRepo extends JpaRepository<Card, Long> {
     List<Card> findCardsByList_IdOrderByPosition(Long id);
-
     void deleteByContent(String content);
-
     @Query(nativeQuery = true, value = "select * from card_tag_user join card on card_tag_user.card_id = card.id join list on card.list_id = list.id join board on list.board_id = board.id where board_id = ? and card_tag_user.app_user_id = ?")
     List<Card> findCardByUserIdAndBroadId(Long broadId, Long userId);
-
     @Query(nativeQuery = true, value = "select * from card")
     List<Card> findAllCard();
-
     @Query(nativeQuery = true, value = "select * from card join card_labels on card.id = card_labels.card_id join list on card.list_id = list.id join board on list.board_id = board.id where labels_id = ?1 and board_id = ?2")
     List<Card> findCardsByLabel(Long labels_id, Long board_id);
 }
