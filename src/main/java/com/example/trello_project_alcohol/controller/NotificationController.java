1bfab406b24bd5ab1a337572b27179279f773557
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-05-04 12:12:19 +0700

    create findNotificationByAppUser in NotificationController

--- src/main/java/com/example/trello_project_alcohol/controller/NotificationController.java
+++ src/main/java/com/example/trello_project_alcohol/controller/NotificationController.java
@@ -1,11 +1,16 @@
 package com.example.trello_project_alcohol.controller;
 
+import com.example.trello_project_alcohol.model.AppUser;
 import com.example.trello_project_alcohol.model.Notification;
 import com.example.trello_project_alcohol.service.notificationService.INotificationService;
+import com.example.trello_project_alcohol.service.userService.IAppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("*")
 @RestController
@@ -13,9 +18,16 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
     @Autowired
     private INotificationService iNotificationService;
+    @Autowired
+    private IAppUserService userService;
 
-    @PostMapping("create")
+    @PostMapping("")
     public ResponseEntity<Notification> create(@RequestBody Notification notification){
         return new ResponseEntity<>(iNotificationService.save(notification), HttpStatus.OK);
+    }
+    @GetMapping("")
+    public ResponseEntity<List<Notification>> findNotificationByUser(){
+        Optional<AppUser> appUser = userService.getUserCurrent();
+        return new ResponseEntity<>(iNotificationService.findNotificationByUser(appUser.get().getId()),HttpStatus.OK);
     }
 }
