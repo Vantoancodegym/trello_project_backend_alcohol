a424b0d522b3d99a44b3d780a2d13abb59a7944b
MungC2010G2 <zhaiduong26z@gmail.com>
2021-05-06 14:56:34 +0700

    create branch breakTagUserCard

--- src/main/java/com/example/trello_project_alcohol/service/CardTagUserService/ICardTagUserService.java
+++ src/main/java/com/example/trello_project_alcohol/service/CardTagUserService/ICardTagUserService.java
@@ -4,4 +4,5 @@ import com.example.trello_project_alcohol.model.Card_tagUser;
 
 public interface ICardTagUserService {
     Card_tagUser create(Card_tagUser card_tagUser);
+    void delete(Long id);
 }
