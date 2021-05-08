281ca37ba8374a4b29a01b1796a2f36231d26ab0
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-05-08 08:59:22 +0700

    add method getListTagUser in BoardTagAppUserService

--- src/main/java/com/example/trello_project_alcohol/service/broad/boardTagAppUser/IBoardTagAppUserService.java
+++ src/main/java/com/example/trello_project_alcohol/service/broad/boardTagAppUser/IBoardTagAppUserService.java
@@ -1,10 +1,11 @@
 package com.example.trello_project_alcohol.service.broad.boardTagAppUser;
 
+import com.example.trello_project_alcohol.model.AppUser;
 import com.example.trello_project_alcohol.model.TagUser_Board;
 import com.example.trello_project_alcohol.service.IService;
 
+import java.util.List;
+
 public interface IBoardTagAppUserService extends IService<TagUser_Board> {
-
-
-
+    List<AppUser> getListTagUser(Long board_id);
 }
